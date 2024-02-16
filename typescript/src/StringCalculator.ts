9170728cf15d3327615502c4665b383e3744ad98
Alberto Morales <kodenix@gmail.com>
2024-02-16 19:39:47 +0100

    Refactor simplified reduce implementation

--- typescript/src/StringCalculator.ts
+++ typescript/src/StringCalculator.ts
@@ -1,13 +1,9 @@
 export class StringCalculator {
   add(numbers: string): number {
     if (numbers === "") return 0;
-    
-    const parts = numbers.split(",");
 
-    const nums = parts.map(val => +val);
-    const sum = nums.reduce((acc, current)=>{
-      return acc + current;
-    }, 0)
-    return sum;
+    return numbers.split(",")
+      .map(val => +val)
+      .reduce((acc, current) => acc + current, 0)
   }
 }
