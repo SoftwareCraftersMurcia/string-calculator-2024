b3ff0212f0efb07a64d41773a70515ec2941360d
Alberto Morales <kodenix@gmail.com>
2024-02-16 19:33:49 +0100

    should return the sum of any amount of numbers separated by comma

--- typescript/src/StringCalculator.ts
+++ typescript/src/StringCalculator.ts
@@ -1,7 +1,18 @@
 export class StringCalculator {
   add(numbers: string): number {
     if (numbers === "") return 0;
+    
     const parts = numbers.split(",");
+
+    
+    if (parts.length > 2) {
+      const nums = parts.map(val => +val);
+      const sum = nums.reduce((acc, current)=>{
+        return acc + current;
+      }, 0)
+      return sum;
+    }
+
     if (parts.length > 1) return +parts[0] + +parts[1];
     return +numbers;
   }
--- typescript/tests/StringCalculator.spec.ts
+++ typescript/tests/StringCalculator.spec.ts
@@ -1,19 +1,23 @@
 import { StringCalculator } from "../src/StringCalculator";
 
 describe("StringCalculator", () => {
-  it("should return 0 for empty string", () => {
-    const strCalc = new StringCalculator();
+  let strCalc: StringCalculator;
+  
+  beforeEach(() => {
+    strCalc = new StringCalculator();
+  })
 
+  it("should return 0 for empty string", () => {
     expect(strCalc.add("")).toBe(0);
   });
   it("should return the numeric value of string with one number", () => {
-    const strCalc = new StringCalculator();
-
     expect(strCalc.add("40")).toBe(40);
   });
   it("should return the sum of two numbers separated by comma", () => {
-    const strCalc = new StringCalculator();
-
     expect(strCalc.add("1,2")).toBe(3);
+  });
+
+  it("should return the sum of any amount of numbers separated by comma", () => {
+    expect(strCalc.add("1,2,3,4,5,6,7,8,9")).toBe(45);
   });
 });
