36b57bb57e45196f46258b0f427f2126deb94bad
marmol89 <38738460+marmol89@users.noreply.github.com>
2024-02-16 18:50:49 +0100

    Refactor value abstract empty logic

--- php/src/StringCalculator.php
+++ php/src/StringCalculator.php
@@ -6,19 +6,6 @@ class StringCalculator
 {
     public function add(string $value): int
     {
-        if ($value === '4') {
-            return 4;
-        }
-
-        if ($value === '5') {
-            return 5;
-        }
-
-        if ($value === '6') {
-            return 6;
-        }
-
-
-        return 0;
+        return (int) $value;
     }
 }
--- php/tests/StringCalculatorTest.php
+++ php/tests/StringCalculatorTest.php
@@ -18,32 +18,12 @@ class StringCalculatorTest extends TestCase
     }
 
     /** @test */
-    public function given_4_value_then_return_4(): void
+    public function given_unique_value_then_return_its_self(): void
     {
         $stringCalculator = new StringCalculator();
 
         $result = $stringCalculator->add("4");
 
         self::assertEquals(4, $result);
-    }
-
-    /** @test */
-    public function given_5_value_then_return_5(): void
-    {
-        $stringCalculator = new StringCalculator();
-
-        $result = $stringCalculator->add("5");
-
-        self::assertEquals(5, $result);
-    }
-
-    /** @test */
-    public function given_6_value_then_return_6(): void
-    {
-        $stringCalculator = new StringCalculator();
-
-        $result = $stringCalculator->add("6");
-
-        self::assertEquals(6, $result);
     }
 }
