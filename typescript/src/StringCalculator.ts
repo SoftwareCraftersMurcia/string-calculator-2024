63fe078c30e18cc5304c71c6a918722e044c96de
Victor Serrano <vsbagile@gmail.com>
2024-02-16 18:56:47 +0100

    should return the numeric value of string with one number

--- typescript/src/StringCalculator.ts
+++ typescript/src/StringCalculator.ts
@@ -1,6 +1,7 @@
 
 export class StringCalculator {
   add(numbers: string): number {
+    if(numbers!=="")return +numbers;    
     return 0;
   }
 }
--- typescript/tests/StringCalculator.spec.ts
+++ typescript/tests/StringCalculator.spec.ts
@@ -6,5 +6,10 @@ describe('StringCalculator', () => {
 
     expect(strCalc.add("")).toBe(0)
   })
+  it('should return the numeric value of string with one number', () => {
+    const strCalc = new StringCalculator()
+
+    expect(strCalc.add("40")).toBe(40)
+  })
 })
 
