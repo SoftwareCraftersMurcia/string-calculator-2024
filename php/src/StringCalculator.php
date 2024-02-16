0dbf79143ed88fc3077654dd7d81967e2613b533
marmol89 <38738460+marmol89@users.noreply.github.com>
2024-02-16 18:57:58 +0100

    green: refactor sum two values

--- php/src/StringCalculator.php
+++ php/src/StringCalculator.php
@@ -6,14 +6,10 @@ class StringCalculator
 {
     public function add(string $value): int
     {
-        if ($value === '1,2') {
-            return 3;
-        }
-
         $numbers = explode(',', $value);
 
         if(count($numbers) > 1) {
-            return $numbers[0] + $numbers[1];
+            return (int) $numbers[0] + (int) $numbers[1];
         }
 
         return (int) $value;
