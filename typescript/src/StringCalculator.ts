2841fb3d907cc61e56564dea0caddbf343351660
Victor Serrano <vsbagile@gmail.com>
2024-02-16 19:00:05 +0100

    refactor: make empty string case an early return

--- typescript/src/StringCalculator.ts
+++ typescript/src/StringCalculator.ts
@@ -1,7 +1,7 @@
 
 export class StringCalculator {
   add(numbers: string): number {
-    if(numbers!=="")return +numbers;    
-    return 0;
+    if(numbers==="")return 0;    
+    return +numbers;
   }
 }
